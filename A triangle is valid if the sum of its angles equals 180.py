51dafd9f12b1d7133417f3a161051771737f26a6
ANUSHK467 <anushk.singh_bca24@gla.ac.in>
2025-09-02 22:04:40 +0530

    Create A triangle is valid if the sum of its angles equals 180.py

---
+++ A triangle is valid if the sum of its angles equals 180.py
@@ -0,0 +1,7 @@
+A = int(input("enter the angle"))
+B = int(input("enter the angle"))
+C = int(input("enter the angle"))
+if A + B + C == 180:
+    print("Valid triangle")
+else:
+    print("Invalid triangle")
