4305eef4f83b8a8f1bdebf9f5fedfbad949c47c7
ANUSHK467 <anushk.singh_bca24@gla.ac.in>
2025-09-02 22:02:13 +0530

    Create Given 5 numbers A, B, C, D, E as input. Print the average of these 5 numbers..py

---
+++ Given 5 numbers A, B, C, D, E as input. Print the average of these 5 numbers..py
@@ -0,0 +1,7 @@
+A = int(input("enter the no"))
+B = int(input("enter the no"))
+C = int(input("enter the no"))
+D = int(input("enter the no"))
+E = int(input("enter the no"))
+avg = (A + B + C + D + E) / 5
+print("Average is:", avg)
